d46789d170ab50bac7c42050b26ade2079842a88
hayden brown <haydenbrowncinci@gmail.com>
2022-02-07 14:18:34 -0500

    added majors

--- EducationProject/Program.cs
+++ EducationProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EducationProject
 {
@@ -6,7 +7,15 @@ namespace EducationProject
     {
         static void Main(string[] args)
         {
-             
+            List<Major> majors = new List<Major>();
+
+            Major business = new Major("Business", 1000);
+            majors.Add(business);
+            Major nursing = new Major("Nursing", 1200);
+            majors.Add(nursing);
+            Major theater = new Major("Theater", 1100);
+            majors.Add(theater);
+
         }
     }
 }
